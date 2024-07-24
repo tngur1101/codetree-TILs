d3427a305d66a59b959f12dc8e03d91f4d54588a
tngur1101 <tngur1101@naver.com>
2024-07-24 21:52:23 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240724/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n1, n2;
+    static int[] n1Arr;
+    static int[] n2Arr;
+    static int[] sliceArr;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        n1 = Integer.parseInt(st.nextToken());
+        n2 = Integer.parseInt(st.nextToken());
+
+        n1Arr = new int[n1];
+        n2Arr = new int[n2];
+        sliceArr = new int[n2];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n1; i++){
+            n1Arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n2; i++){
+            n2Arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int startIdx = getStartIdx();
+        if(startIdx != -1){
+            putArr(startIdx);
+            if(isAnswer()) System.out.println("Yes");
+            else System.out.println("No");
+        } else {
+            System.out.println("No");
+        }
+
+    }
+
+    private static int getStartIdx(){
+        for(int i = 0; i < n1Arr.length; i++){
+            if(n1Arr[i] == n2Arr[0]) return i;
+        }
+
+        return -1;
+    }
+
+    private static boolean isAnswer(){
+        for(int i = 0; i < n2Arr.length; i++){
+            if(n2Arr[i] != sliceArr[i]) return false;
+        }
+
+        return true;
+    }
+
+    private static void putArr(int idx){
+        for(int i = 0; i < n2Arr.length; i++){
+            sliceArr[i] = n1Arr[idx++];
+        }
+    }
+}
