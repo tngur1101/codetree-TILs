ed3b90eeab21b3828f2790c78e843f67a60cf6d8
tngur1101 <tngur1101@naver.com>
2024-07-26 15:06:46 +0900

    Title: 반복 출력하기 2, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240726/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        printHelloWorld(n);
+    }
+
+    private static void printHelloWorld(int depth){
+        if(depth == 0){
+            return;
+        }
+
+        printHelloWorld(depth - 1);
+        System.out.println("HelloWorld");
+    }
+}
