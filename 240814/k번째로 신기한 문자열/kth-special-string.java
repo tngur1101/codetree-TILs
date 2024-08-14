72b1b6dbf308bd346f89be569eb63c3bc5cbc492
tngur1101 <tngur1101@naver.com>
2024-08-14 16:59:17 +0900

    Title: k번째로 신기한 문자열, Time: 103ms, Memory: 9MB, Status: Passed - CodeTree

--- 240814/k번째로 신기한 문자열/kth-special-string.java
+++ 240814/k번째로 신기한 문자열/kth-special-string.java
@@ -7,35 +7,31 @@ public class Main {
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken())-1;
-        String T = st.nextToken();
+        int k = Integer.parseInt(st.nextToken()) - 1;
+        String t = st.nextToken();
 
-        char[] t = T.toCharArray();
-        // System.out.println(t.length);
-
-        boolean flag = true;
+        String[] strArr = new String[n];
         ArrayList<String> list = new ArrayList<>();
 
         for(int i = 0; i < n; i++){
-            String temp = br.readLine();
-            char[] tempArr = temp.toCharArray();
-            flag = true;
-
-            for(int j = 0; j < t.length; j++){
-                if(t[j] != tempArr[j]) flag = false;
+            strArr[i] = br.readLine();
+            if(startsWith(strArr[i], t)){
+                list.add(strArr[i]);
             }
-
-            if(flag) list.add(temp);
         }
 
         Collections.sort(list);
-
-        // for(int i = 0; i < list.size(); i++){
-        //     System.out.println(list.get(i));
-        // }
         System.out.println(list.get(k));
 
+    }
+
+    private static boolean startsWith(String a, String b){
+        if((int) a.length() < (int) b.length()) return false;
+        for(int i = 0; i < (int) b.length(); i++){
+            if(a.charAt(i) != b.charAt(i)) return false;
+        }
+
+        return true;
     }
 }
