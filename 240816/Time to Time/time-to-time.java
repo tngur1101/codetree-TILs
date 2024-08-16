796b825b6670ccea7e3f7232655c59029cfa4dec
tngur1101 <tngur1101@naver.com>
2024-08-16 19:40:51 +0900

    Title: Time to Time, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240816/Time to Time/time-to-time.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int start = a*60 + b;
+        int end = c*60+d;
+
+        System.out.println(end - start);
+
+    }
+}
