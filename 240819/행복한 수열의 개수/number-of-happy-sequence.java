323d7d9d734d44c43baa312378237615f678459f
tngur1101 <tngur1101@naver.com>
2024-08-19 17:29:11 +0900

    Title: 행복한 수열의 개수, Time: 123ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240819/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+/*
+* 행복한 수열 = 연속하여 m개 이상의 동일한 원소가 나오는 순간이 존재하는 수열
+* 
+*/
+
+public class Main {
+    static int N,M;
+    static int[][] map;
+    static int ans;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < N; i++){
+            checkRow(i);
+            checkColumn(i);
+        }
+        System.out.println(ans);
+    }
+
+    // 행을 체크하는 함수
+    // r: 체크할 행의 인덱스
+    private static void checkRow(int r){
+        int cnt = 1;
+        for(int i = 0; i < N-1; i++){
+            if(map[r][i] == map[r][i+1]) cnt++;
+            else cnt=1;
+        }
+        if(cnt >= M) ans++;
+    }
+
+    // 열을 체크하는 함수
+    // c : 체크할 열의 인덱스
+    private static void checkColumn(int c){
+        int cnt = 1;
+        for(int i = 0; i < N-1; i++){
+            if(map[i][c] == map[i+1][c]) cnt++;
+            else cnt=1;
+        }
+        if(cnt >= M) ans++;
+    }
+}
