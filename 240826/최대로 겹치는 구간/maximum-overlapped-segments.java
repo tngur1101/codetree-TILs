8b550a1e42a068d4fda6e01e3a450037f2fd9ae7
tngur1101 <tngur1101@naver.com>
2024-08-26 14:57:01 +0900

    Title: 최대로 겹치는 구간, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

--- 240826/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240826/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -12,12 +12,12 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
 
-        arr = new int[101];
+        arr = new int[201];
 
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken())+100;
+            int x2 = Integer.parseInt(st.nextToken())+100;
 
             for(int x = x1; x < x2; x++){
                 arr[x] += 1;
